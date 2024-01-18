af1219e187d80d64097d537046aaf62d7498607e
Ghita Aachab <ghita.aachab@gmail.com>
2024-01-18 17:11:00 +0100

    watchlist rails

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,13 +13,16 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save
-    redirect_to lists_path(@list)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def list_params
-    params.require(:list).permit(:content)
+    params.require(:list).permit(:name)
   end
 end
