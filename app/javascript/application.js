bbee3fa260c5a4b1f392c9a2a4a429976ec7071a
Ghita Aachab <ghita.aachab@gmail.com>
2024-01-18 12:46:23 +0100

    rails watch list

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
+// app/javascript/application.js
+import "bootstrap"
+import "@popperjs/core"
+
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
