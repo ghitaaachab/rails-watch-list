fe57c6ff7af0e51cf286e142f7c28eb7f207dd1c
Ghita Aachab <ghita.aachab@gmail.com>
2024-01-18 22:41:18 +0100

    rails wishlist

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :lists do
-    resources :bookmarks             #to nest bookmarks inside lists /lists/id/new(bookmark)
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create] #to nest bookmarks inside lists /lists/id/new(bookmark)
   end
   resources :bookmarks, only: [:destroy]
-  resources :movies
   # Defines the root path route ("/")
   # root "posts#index"
 end
